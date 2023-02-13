e289dfb3f2eeac231365d86c26ee50fd518dc5f8
Dhiraj Karki <karkidhiraj56@gmail.com>
2023-02-13 14:10:17 +0545

    Completed

---
+++ Dhiraj.java
@@ -0,0 +1,14 @@
+public class Dhiraj{
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Dhiraj Restaurant and Bar");
+
+        System.out.println("Address:");
+        System.out.println("----State: Bagmati 10");
+        System.out.println("----Zip Code: 1111");
+        System.out.println("----Country: Nepal");
+
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9806471416");
+        System.out.println("----Email Address: karkidhiraj56@gmail.com");
+    }
+}
